9e6880ece9ca0c734e62d61e58bce088987802b1
Malte Pedersen <maltebp94@gmail.com>
2022-01-23 19:16:35 +0100

    ??

--- src/CppHashMap/HashMap.h
+++ src/CppHashMap/HashMap.h
@@ -1,15 +1,29 @@
 #pragma once
 
 
+template<typename Key, typename Value>
 class HashMap {
-public:
 
-    
+    struct Entry;
 
+public: 
 
 
 
 private:
 
+    Entry** buckets = nullptr;
+    
+    size_t numBuckets = 0;
+    
+    float maxLoadFactor = 1f;
+
+private:
+
+    struct Entry {
+        Entry*  next = nullptr;
+        Key key;
+        Value value;
+    };
 
 };
